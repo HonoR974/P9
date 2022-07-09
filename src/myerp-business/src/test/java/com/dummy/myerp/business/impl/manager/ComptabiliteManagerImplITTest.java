99694ee0fad6b07e332cab1c78ffa8564aa8415c
HonoR974 <honore.guillaudeau1@gmail.com>
2022-07-09 20:14:06 +0200

    Configuration Problems 2

--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplITTest.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplITTest.java
@@ -7,26 +7,9 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.dummy.myerp.business.impl.manager.ComptabiliteManagerImpl;
-import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
-
-import java.math.BigDecimal;
-import java.util.Date;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
-import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
-import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
-import com.dummy.myerp.technical.exception.FunctionalException;
-import com.dummy.myerp.technical.exception.NotFoundException;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/testContext.xml" })
-public class ComptabiliteManagerImplITTest {
+public class ComptabiliteManagerImplITTest extends BusinessTestCase {
 
         private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
 
---
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/TestInitSpring.java
@@ -0,0 +1,28 @@
+package com.dummy.myerp.business.impl.manager;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+/**
+ * Classe de test de l'initialisation du contexte Spring
+ */
+public class TestInitSpring extends BusinessTestCase {
+
+    /**
+     * Constructeur.
+     */
+    public TestInitSpring() {
+        super();
+    }
+
+    /**
+     * Teste l'initialisation du contexte Spring
+     */
+    @Test
+    public void testInit() {
+        SpringRegistry.init();
+        assertNotNull(SpringRegistry.getBusinessProxy());
+        assertNotNull(SpringRegistry.getTransactionManager());
+    }
+}
